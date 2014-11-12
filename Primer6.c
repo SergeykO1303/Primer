4a544f867a4ada9643ccea5f92054d4a4a5b1f99
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-11-12 19:01:32 +0900

    Primeeer

---
+++ Primer6.c
@@ -0,0 +1,31 @@
+/****************************************************************************
+ *                                                                          *
+ * File    : main.c                                                         *
+ *                                                                          *
+ * Purpose : Console mode (command line) program.                           *
+ *                                                                          *
+ * History : Date      Reason                                               *
+ *           00/00/00  Created                                              *
+ *                                                                          *
+ ****************************************************************************/
+
+#include <stdio.h>
+
+/****************************************************************************
+ *                                                                          *
+ * Function: main                                                           *
+ *                                                                          *
+ * Purpose : Main entry point.                                              *
+ *                                                                          *
+ * History : Date      Reason                                               *
+ *           00/00/00  Created                                              *
+ *                                                                          *
+ ****************************************************************************/
+
+int main(int argc, char *argv[])
+{
+	char string[]= "Wawilon";
+    printf("Hello, world! %s. \n",string);
+    return 0;
+}
+
---
+++ Primer7.c
@@ -0,0 +1,33 @@
+/****************************************************************************
+ *                                                                          *
+ * File    : main.c                                                         *
+ *                                                                          *
+ * Purpose : Console mode (command line) program.                           *
+ *                                                                          *
+ * History : Date      Reason                                               *
+ *           00/00/00  Created                                              *
+ *                                                                          *
+ ****************************************************************************/
+
+#include <stdio.h>
+
+/****************************************************************************
+ *                                                                          *
+ * Function: main                                                           *
+ *                                                                          *
+ * Purpose : Main entry point.                                              *
+ *                                                                          *
+ * History : Date      Reason                                               *
+ *           00/00/00  Created                                              *
+ *                                                                          *
+ ****************************************************************************/
+
+int main(int argc, char *argv[])
+{
+	char string[999];
+	printf("Please, write me name \a");
+	scanf ("%s" ,&string);
+    printf("Hello,\a %s. \n",string);
+    return 0;
+}
+
